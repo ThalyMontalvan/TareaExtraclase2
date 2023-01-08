47f518832866bdf8078d6fa21f8d6216686a3bb8
ThalyMontalvan <117538574+ThalyMontalvan@users.noreply.github.com>
2023-01-07 23:53:45 -0500

    tarea modificada

---
+++ Tarea_Extraclase/src/Tarea_Extraclase/ejercicio_llave1.java
@@ -0,0 +1,11 @@
+
+package Tarea_Extraclase;
+
+public interface ejercicio_llave1 {
+    
+    public void setEquipoLocal(String NombreEquipo);
+    
+    public void setEquipoVisitante(String NombreEquipo);
+    }
+    
+
